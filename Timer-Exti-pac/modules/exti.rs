054ae6ffb66c91b364d17725e30a52e1da9e24af
Tanishk Yadav <sprabha430@gmail.com>
2026-08-04 21:50:57 +0530

    Improve comments in exti.rs for better readability
    Refactored comments for clarity and consistency throughout the code.

--- Timer-Exti-pac/modules/exti.rs
+++ Timer-Exti-pac/modules/exti.rs
@@ -1,37 +1,49 @@
-use stm32l476_pac::Exti; /*Brought EXTI, SYSCFG, RCC, GPIOC & NVIC into scope from PAC & cortex_m*/
+/*Brought EXTI, SYSCFG, RCC, GPIOC & NVIC into scope from PAC & cortex_m*/
+use stm32l476_pac::Exti; 
 use stm32l476_pac::Syscfg;
 use stm32l476_pac::Rcc;
 use stm32l476_pac::Gpioc;
 use cortex_m::peripheral::NVIC;
-use core::sync::atomic::{AtomicU8,Ordering}; /*Brought ATOMIC u8 in scope for preventing race-conditions with atomic variable writes*/
-
-pub static COUNTER:AtomicU8 = AtomicU8::new(0); /*Global atomic variable for no. of button presses*/
-
-pub fn gpio_init(rcc:&Rcc,c:&Gpioc){ /*Func. for Initialisng PC0 for EXTI-line*/
-    rcc.ahb2enr().modify(|_r,w| w.gpiocen().set_bit()); /*Enabling Clock of GPIO Port C on AHB-2 bus*/
-    rcc.apb2enr().modify(|_r,w| w.syscfgen().set_bit()); /*Enabling Clock of SYSCFG on APB-2 bus*/
-    c.moder().modify(|_r,w| unsafe{w.moder0().bits(0b00)}); /*Setting PC0 to Input mode*/
-    c.pupdr().modify(|_r,w| unsafe{w.pupdr0().bits(0b01)}); /*Internal 40kΩ pull-up for Active-High*/
+/*Brought ATOMIC u8 in scope for preventing race-conditions with atomic variable writes*/
+use core::sync::atomic::{AtomicU8,Ordering}; 
+/*Global atomic variable for no. of button presses*/
+pub static COUNTER:AtomicU8 = AtomicU8::new(0); 
+/*Function for Initialisng PC0 for EXTI-line*/
+pub fn gpio_init(rcc:&Rcc,c:&Gpioc){ 
+/*Enabling Clock of GPIO Port C & SYSCFG*/
+    rcc.ahb2enr().modify(|_r,w| w.gpiocen().set_bit());
+    rcc.apb2enr().modify(|_r,w| w.syscfgen().set_bit()); 
+/*Configuring PC0 for Input mode with 40kΩ internal pull-up*/    
+    c.moder().modify(|_r,w| unsafe{w.moder0().bits(0b00)}); 
+    c.pupdr().modify(|_r,w| unsafe{w.pupdr0().bits(0b01)});
 }
-
-pub fn button_init(sys:&Syscfg,ex:&Exti,nv:&mut NVIC){ /*Func. for Configuring EXTI0 for button interrupt*/
-    sys.exticr1().modify(|_r,w| unsafe{w.exti0().bits(0b0010)}); /*Mapping EXTI0 to Port C as EXTI no. denotes pin no. and SYSCFG maps to port*/
-    ex.ftsr1().modify(|_r,w| w.tr0().set_bit()); /*Falling edge trigger as pin goes low when button is pressed*/
+/*Function for Configuring EXTI0 for button interrupt*/
+pub fn button_init(sys:&Syscfg,ex:&Exti,nv:&mut NVIC){ 
+/*Mapping EXTI0 to Port C as EXTI no. denotes pin no. and SYSCFG maps to port*/
+    sys.exticr1().modify(|_r,w| unsafe{w.exti0().bits(0b0010)}); 
+/*Falling edge trigger as pin goes low when button is pressed*/
+    ex.ftsr1().modify(|_r,w| w.tr0().set_bit());
     ex.rtsr1().modify(|_r,w| w.tr0().clear_bit()); /*No rising edge trigger*/
-    ex.imr1().modify(|_r,w| w.mr0().set_bit()); /*Unmasking EXTI0*/
+/*Unmasking EXTI0*/
+    ex.imr1().modify(|_r,w| w.mr0().set_bit());
+/*Unmasking EXTI for the Interrupt controller with 0x00 as its priority*/
     unsafe{
-        NVIC::unmask(stm32l476_pac::Interrupt::EXTI0); /*Unmasking EXTI for the Interrupt controller*/
-        nv.set_priority(stm32l476_pac::Interrupt::EXTI0, 0x00); /*Asssigning 0x00 as Priority to its Interrupt*/
+        NVIC::unmask(stm32l476_pac::Interrupt::EXTI0); 
+        nv.set_priority(stm32l476_pac::Interrupt::EXTI0, 0x00);
     }
 }
 #[no_mangle]
 pub extern "C" fn EXTI0(){ /*EXTI0_IRQHandler (ISR)*/
-   let ext0=unsafe{stm32l476_pac::Peripherals::steal()}; /*'ext0 contains the REG address of MCU peripherals*/
-   if ext0.exti.pr1().read().pr0().bit_is_set() { /*Checking if the pending bit is set when Interrupt triggers*/
-      ext0.exti.pr1().write(|w| w.pr0().set_bit()); /*Setting PR0 bit clears the flag*/
-      COUNTER.fetch_add(1,Ordering::SeqCst); /*Atomically incrementing COUNTER by 1 each time*/
+/*'mp' contains the REG address of MCU peripherals*/    
+   let mp=unsafe{stm32l476_pac::Peripherals::steal()}; 
+   if mp.exti.pr1().read().pr0().bit_is_set() { /*Checking if the pending bit is set when Interrupt triggers*/     
+/*Setting PR0 bit clears the flag*/       
+      mp.exti.pr1().write(|w| w.pr0().set_bit()); 
+/*Atomically incrementing COUNTER by 1 each time*/       
+      COUNTER.fetch_add(1,Ordering::SeqCst); 
       if COUNTER.load(Ordering::SeqCst)==4 { 
-        COUNTER.store(0,Ordering::SeqCst); /*Resets COUNTER value after completing all 4-states*/
+/*Resets COUNTER value after completing all 4-states*/          
+        COUNTER.store(0,Ordering::SeqCst); 
       }
    }
 }
