a1d1835388f773cd3aec0d76b59919864fdd1cb0
Tanishk Yadav <sprabha430@gmail.com>
2026-08-04 21:40:02 +0530

    Refactor main.rs for clarity and organization

--- Timer-Exti-pac/Main.rs
+++ Timer-Exti-pac/Main.rs
@@ -2,43 +2,60 @@
 #![no_main]
 
 use panic_halt as _;
-use stm32l476_pac::{Peripherals, Tim2}; /*Importing all the Peripherals from the Peripheral Access Crate generated from SVD*/
-use cortex_m_rt::entry; /*Importing Entry point for the program for executing main*/
-mod timer; /*Including timer.rs module containing timer-2 configurations and functions*/
-mod exti; /*Including exti.rs module containing EXTI_0 configurations and functions for PC0*/
-use exti::COUNTER; /*Signifies the no. of button presses*/
-use timer::TOGGLE; /*Flag set upon execution of the Timer-2 ISR*/
-
-enum Speed{ /*Defined Speed Variants for State Machine structure*/
+/*Importing all the Peripherals from the Peripheral Access Crate generated from SVD*/
+use stm32l476_pac::{Peripherals, Tim2}; 
+/*Importing Entry point for the program for executing main*/
+use cortex_m_rt::entry; 
+/*Including timer.rs module containing timer-2 configurations and functions*/
+mod timer; 
+/*Including exti.rs module containing EXTI_0 configurations and functions for PC0*/
+mod exti; 
+/*Signifies the no. of button presses*/
+use exti::COUNTER; 
+/*Flag set upon execution of the Timer-2 ISR*/
+use timer::TOGGLE; 
+/*Defined Speed Variants for State Machine structure*/
+enum Speed{ 
   Slow,
   Medium,
   High,
   VeryHigh
 }
-
-#[entry] /*'entry' attributes indicates start of main function*/
+/*'entry' attributes indicates start of main function*/
+#[entry] 
 fn main()->!{
   let timconfig=timer::TIM{ /*Assigning values to TIM struct variables from timer.rs*/
     psc:3999, 
     arr:1000
   };
-  let dp = unsafe{Peripherals::steal()}; /*'dp' contains the REG addresses of MCU Peripherals struct from the PAC*/
-  let mut cp=unsafe{cortex_m::Peripherals::steal()}; /*'cp' contains the REG addresses of Cortex-M Peripherals*/
-  dp.rcc.ahb2enr().modify(|_r, w| w.gpioaen().set_bit()); /*Enabling Clock of GPIO Port-A on AHB-2 bus*/
-  dp.gpioa.moder().modify(|_r, w| unsafe{ w.moder0().bits(0b01) }); //*Setting PA0 to General Purpose Output Mode */
-  exti::gpio_init(&dp.rcc,&dp.gpioc); /*Initialising PC0 for EXTI0 Line*/
-  exti::button_init(&dp.syscfg,&dp.exti,&mut cp.NVIC); /*Configuring EXTI0*/
-  timconfig.timer_init(&dp.tim2,&dp.rcc,&mut cp.NVIC); /*Configuring Timer-2*/
+  /*'dp' contains the REG addresses of MCU Peripherals struct from the PAC*/
+  let dp = unsafe{Peripherals::steal()}; 
+  /*'cp' contains the REG addresses of Cortex-M Peripherals*/
+  let mut cp=unsafe{cortex_m::Peripherals::steal()}; 
+  /*Enabling Clock of GPIO Port-A on AHB-2 bus*/
+  dp.rcc.ahb2enr().modify(|_r, w| w.gpioaen().set_bit());
+  //*Setting PA0 to General Purpose Output Mode */
+  dp.gpioa.moder().modify(|_r, w| unsafe{ w.moder0().bits(0b01) }); 
+  /*Initialising PC0 for EXTI0 Line*/
+  exti::gpio_init(&dp.rcc,&dp.gpioc); 
+   /*Configuring EXTI0*/
+  exti::button_init(&dp.syscfg,&dp.exti,&mut cp.NVIC);
+  /*Configuring Timer-2*/
+  timconfig.timer_init(&dp.tim2,&dp.rcc,&mut cp.NVIC); 
   loop{
-    if TOGGLE.swap(false,core::sync::atomic::Ordering::SeqCst) { /*Checking if Timer-2 ISR flag is set and atomically setting it to False*/
-      Speed::state_machine( &dp.tim2); /*Deploying State-Machine method from Speed impl block*/
-      dp.gpioa.odr().modify(|_r,w| unsafe{w.bits(_r.bits() ^ (1u32))}); /*Toggling PA0 (LED) according to the current delay*/
+    /*Checking if Timer-2 ISR flag is set and atomically setting it to False*/
+    if TOGGLE.swap(false,core::sync::atomic::Ordering::SeqCst) { 
+      /*Deploying State-Machine method from Speed impl block*/
+      Speed::state_machine(&dp.tim2); 
+      /*Toggling PA0 (LED) according to the current delay*/
+      dp.gpioa.odr().modify(|_r,w| unsafe{w.bits(_r.bits() ^ (1u32))}); 
     }
   }
 }
 
 impl Speed{ /*Method implementations for Speed enum*/
-  fn count()->Speed{ /*Function for assigning COUNTER value to Enum variants & returning the corresponding variant*/
+  /*Function for assigning COUNTER value to Enum variants & returning the corresponding variant*/
+  fn count()->Speed{ 
     match COUNTER.load(core::sync::atomic::Ordering::SeqCst) { 
       0=>Speed::Slow, /*COUNTER values mapped directly to different Blink Speed*/
       1=>Speed::Medium,
